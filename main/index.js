7710a728d18b692009c293b832737b7194f3b308
YanHeng Zhou <1801084586@qq.com>
2025-04-10 15:41:33 +0800

    feat:git

--- main/index.js
+++ main/index.js
@@ -1,7 +1,4 @@
-/*
- * @Author: huayou.fu 
- * @Created date: 2022-01-04 16:46:13 
- */
+
 import React from "react";
 import { createRootStack } from "./navigate";
 export default class App extends React.PureComponent {
